61b8fc05f953628ead7d668d799339681152a74d
adamjuwig <adamjuwig@gmail.com>
2021-02-16 10:54:26 -0700

    temp code added

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,44 @@
 // TODO: Write code to define and export the Employee class
+const Employee = require("./Employee");
+const inquirer = require(".inquirer");
+
+class Employee {
+    constructor (name, id, email) {
+        this.name = name;
+        this.role = role;
+        this.id = id;
+        this.email = email;
+        this.OfficeNumber = OfficeNumber;
+        this.GitHub = GitHub;       
+    }
+    getName() {
+        console.log("Name:", $(this.name));
+        return this.name;
+    }
+    getId() {
+        console.log("Id:", $(this.id));
+        return this.id;
+    }
+    getRole() {
+        console.log("Role:", $(this.employee));
+        return this.role;
+    }
+    getEmail() {
+        console.log("Email:", $(this.email));
+        return this.email;
+    }    
+    getOfficeNumber() {
+        console.log("Office Number:", $(this.OfficeNumber));
+        return this.OfficeNumber;
+    }
+    getGitHub() {
+        console.log("GitHub:", $(this.GitHub));
+        return this.GitHub;
+    }
+    getSchool() {
+        console.log("School:", $(this.school));
+        return this.school;
+    }
+}
+
+module.exports = Employee;
--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,33 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+const inquirer = require(".inquirer");
+
+class Manager extends Employee {
+    constructor(name, id, email, OfficeNumber) {
+        super(name, id, email)
+        this.OfficeNumber = OfficeNumber;
+        this.role = "Manager";
+    }
+    getName() {
+            console.log("Name:", $(this.name));
+            return this.name;
+        }
+    getRole() {
+        console.log("Role: Intern");
+        return "Intern";
+    }
+    getId() {
+        console.log("Id:", $(this.id));
+        return this.id;
+    }    
+    getEmail() {
+        console.log("Email:", $(this.email));
+        return this.email;
+    }    
+    getOfficeNumber() {
+        console.log("Office Number:", $(this.OfficeNumber))
+        return this.OfficeNumber;
+    }
+}
+
+module.exports = Manager;
